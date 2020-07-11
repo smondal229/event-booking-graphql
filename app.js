9b84decc54a2d623c7bd069016fbe3755603d0bc
suvodipCN <smondal229@gmail.com>
2020-07-11 21:44:16 +0530

    basic setup for graphql

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const events = [];
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String
+    }
+
+    type RootQuery {
+      events: [Event!]!
+    }
+
+    type RootMutation {
+      createEvent(eventInput: EventInput): Event
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: ({ eventInput: { title, description, price } }) => {
+        const event = {
+          _id: Math.random().toString(),
+          title,
+          description,
+          price: +price,
+          date: new Date().toISOString()
+        };
+
+        events.push(event);
+        return event;
+      }
+    },
+    graphiql: true
+  })
+);
+
+app.get('/', (req, res, next) => {
+  // console.log(res);
+  res.send('Hello World');
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
